aa5b260d45503a221e3f7a4be368f3ef87bdfb4a
AlexPHPower <bearhug@alexpower.co.uk>
2024-04-14 13:50:59 +0100

    Added about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,28 +3,76 @@
 import {Card, CardContent, CardHeader} from "@/components/ui/card";
 import Image from "next/image";
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
-export default function Home() {
+export default function About() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-12">
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full">
-                <Card>
-                    <CardContent className="flex flex-col md:flex-row items-center p-4">
-                        <div className="md:w-1/2 mt-4 md:mt-0">
-                            <Image src="/PaypalLogo.png" alt="Paypal Logo"
-                                   className="mt-8 mx-auto w-auto h-24"
-                                   width={100}
-                                   height={24}/>
-                        </div>
-                        <div>
-                            <CardHeader className="text-2xl font-bold text-white text-center md:text-left">
-                                Notice Me <span className="text-primary font-extrabold">Senpai</span> (Rick Whalley)
-                            </CardHeader>
-                            <p className="text-neutral-400 text-center md:text-left ml-6">
-                                Customise your dashboard and improve your performance with our reporting tools.
-                            </p>
-                        </div>
+        <main className="">
+            <div className="min-h-screen p-12 flex flex-col items-center justify-between w-full h-full">
+                <Card className="flex flex-col items-center justify-center border-transparent">
+                    <CardContent className="p-4 items-center w-10/12">
+                        <CardHeader className="text-4xl font-bold text-center">
+                            Who is <span className="font-extrabold text-primary">Reign</span>
+                        </CardHeader>
+                        <CardHeader className="text-2xl font-bold text-center">
+                            Why We Started
+                        </CardHeader>
+                        <p className="text-neutral-400 text-left">
+                            We, the founders, felt that while Ranked games were engaging, they lacked a certain
+                            thrill—the excitement of playing for more than just points, but for prizes. There wasn't a
+                            platform where we could pay a modest monthly fee to compete in a well-organized league that
+                            matched us with players of similar skill levels.
+                        </p>
+                        <CardHeader className="text-2xl font-bold text-center">
+                            Why We're Different
+                        </CardHeader>
+                        <p className="text-neutral-400 text-left">
+                            Reign isn't just another competitive league. It's a community where you can consistently
+                            measure your skills against equally matched opponents. Our league uses a subscription model,
+                            making it easy to join and start competing. Whether you’re looking to dominate the
+                            leaderboards or just improve your gameplay, Reign offers a structured environment to do so.
+                        </p>
                     </CardContent>
+                    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full p-20">
+                        <Card className="mb-4 flex flex-col md:flex-row items-center p-4">
+                            <div>
+                                <Tilt perspective={700}>
+                                    <Image src="/Rick.png" alt="Rick Whalley" className="mx-auto rounded-full"
+                                           width={200} height={200} priority/>
+                                </Tilt>
+                                <CardHeader className="text-2xl font-bold text-white text-center">
+                                    Notice Me Senpai <span className="text-primary font-extrabold">(Rick Whalley)</span>
+                                </CardHeader>
+                                <p className="text-neutral-400 text-center md:text-left ml-6 mb-6">
+                                    A Software Consultant whose enthusiasm for gaming matches his expertise in
+                                    engineering. A die-hard fan of Apex, Path of Exile, and Quake, he brings strategic
+                                    thinking from his favorite games into every match. When he's not dominating in the
+                                    gaming world, you can find him whipping up delicious meals or tuning up his car.
+                                    He's a perfect blend of tech-savvy and hands-on skills.
+                                </p>
+                            </div>
+                        </Card>
+                        <Card>
+                            <CardContent className="flex flex-col md:flex-row items-center p-4">
+                                <div>
+                                    <Tilt perspective={700}>
+                                        <Image src="/Biscuit.png" alt="Alex Power" className="mx-auto rounded-full"
+                                               width={200} height={200} priority/>
+                                    </Tilt>
+                                    <CardHeader className="text-2xl font-bold text-white text-center">
+                                        Biscuit <span className="text-primary font-extrabold">(Alex Power)</span>
+                                    </CardHeader>
+                                    <p className="text-neutral-400 text-center md:text-left ml-6 mb-6">
+                                        A software engineer with a knack for strategy and competition.
+                                        Whether he’s outsmarting opponents in Apex Legends, mastering tactics in
+                                        Hearthstone, or building empires in Sid Meier's Civilization, he’s always game
+                                        for a challenge. Off the virtual battlefield, he enjoys exploring new recipes
+                                        and expanding his knowledge.
+                                    </p>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </Card>
             </div>
         </main>
